65cc0241593d83a4c987d70a53867a5e5897cb7d
Daniel <danel.filipek@gmail.com>
2022-11-06 15:03:04 +0100

    Init commit

---
+++ src/main/java/com/qsspy/graphtoolmetricbuilder/GraphToolMetricBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.qsspy.graphtoolmetricbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphToolMetricBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphToolMetricBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsspy/graphtoolmetricbuilder/GraphToolMetricBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsspy.graphtoolmetricbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphToolMetricBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
